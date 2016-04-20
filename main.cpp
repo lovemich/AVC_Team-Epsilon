8cb70e846ba9ca6786ece026143be0c1092f3d2d
Devon Mortimer <devon.mortimer@gmail.com>
2016-04-20 12:00:14 +1200

    Added beginning of movement code

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <string.h>
 
 extern "C" {
-    int init(); // InitHardware()
-    int read_analog(int ch_adc); // ReadAnalog(int ch_adc)
-    int Sleep(int sec, int usec);
+  int init(); // InitHardware()
+  int read_analog(int ch_adc); // ReadAnalog(int ch_adc)
+  int set_motor(int ch_motor, int dir, int speed);
+  int Sleep(int sec, int usec);
 }
 
+// 100% at 255
+int speed_base = 127;
+int speed_delta_m1 = 0;
+int speed_delta_m2 = 0;
+
 int main() {
-    init();
-    return 0;
+  init();
+  return 0;
+}
+
+void move() {
+  set_motor(1,  1,  speed_base + speed_delta_m1);
+  set_motor(2,  1,  speed_base + speed_delta_m2);
 }

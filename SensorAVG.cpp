d8af448c2ce8756543c60c4b628ac76e6e26920b
Portadiam <Portadiam@users.noreply.github.com>
2016-04-27 17:19:17 +1200

    Corrected outdated function declarations
    Corrected outdated function declarations by including extern.h

--- SensorAVG.cpp
+++ SensorAVG.cpp
@@ -1,15 +1,14 @@
+#include "extern.h"
+
 /**
  * Returns an averaged sensor value to deal with noise 
  */
-
-extern "C" int InitHardware(); 
-extern "C" int ReadAnalog(int ch_adc); 
 int getSensorAVG(int sensor, int total){  
-	InitHardware();  
+	init();  
 	int adc_reading;  
 	int count = 0; 
 	while (count < total) {   
-		adc_reading += ReadAnalog(sensor);   
+		adc_reading += read_analog(sensor);   
 		count++;  
 	}  
 	adc_reading /= total;  

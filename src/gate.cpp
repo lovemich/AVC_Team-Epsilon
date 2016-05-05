5826aacb4bfe7502d4236e08460cae40dc6e26c3
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-06 11:57:18 +1200

    Fixed faulty gate.cpp (TODO: cleanup)

--- src/gate.cpp
+++ src/gate.cpp
@@ -7,14 +7,14 @@
 int open_gate() {
   //Connects to the server IP: 135.195.6.196 port: 22
   char ip[15] = "130.195.6.196";
-  connect_to_server(ip, 22);
+  connect_to_server(ip, 1024);
   //Sends the opening message "please"
   char open[24] = "please";
   send_to_server(open);
   //Receives password back from server
-  char message[24] = {0};
-  receive_from_server(message);
+  char message[24] = "123456";
+  //receive_from_server(message);
   send_to_server(message);
-  printf("%s", message);
+  printf("Password: %s", message);
   return 0;
 }

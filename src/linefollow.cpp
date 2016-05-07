770c9b920cf641eef91c5c43523079172a5176f0
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-07 18:30:44 +1200

    Added integral portion of PID (untested)

--- src/linefollow.cpp
+++ src/linefollow.cpp
@@ -6,10 +6,11 @@
  * Makes the robot attempt to follow a line, halting should it lose it.
  */
 void follow_line() {
-  while (true) {
+  int integral = 0;
+  for (int i = 0; true; i++) {
     take_picture();
     int white_count = 0;
-    int movement = 0;
+    int error = 0;
     // Get pixels from across the entire image, not just one line
     for (int x = 0; x < IMAGE_SIZE_X; x += SAMPLE_STEPS) {
       for (int y = 0; y < IMAGE_SIZE_Y; y += SAMPLE_STEPS) {
@@ -18,10 +19,10 @@ void follow_line() {
         if (pixel_value > STOP_THRESHOLD) {
           // Add to counter if a pixel is 'white enough' to be part of a line
           white_count++;
-          // Now weigh the pixel into the movement counter
+          // Now weigh the pixel into the error
           // I'm assuming y increases downwards when weighing
           weight = y > (IMAGE_SIZE_Y / 2) ? 2 : 1;
-          movement += (i - IMAGE_SIZE_X / 2) * weight;
+          error += (i - IMAGE_SIZE_X / 2) * weight;
         }
       }
     }
@@ -30,10 +31,19 @@ void follow_line() {
       break;
     }
     // Calculate how much to turn by
-    int pixels_x = x / SAMPLE_STEPS;
-    int pixels_y = (3 * (y / 2)) / SAMPLE_STEPS;
-    int movement_amount = (movement / (pixels_x * pixels_y)) * K_P;
-    turn(movement_amount);
+    int pixels_x = IMAGE_SIZE_X / SAMPLE_STEPS;
+    int pixels_y = (3 * (IMAGE_SIZE_Y / 2)) / SAMPLE_STEPS;
+    int proportional_error = error / (pixels_x * pixels_y);
+    int proportional_integral = i == 0 ? 0 : integral / i;
+    int movement;
+    // Start with the proportional component;
+    movement = proportional_error * K_P;
+    // Add the integral component
+    movement += integral * K_I;
+    // Update integral component
+    integral += proportional_error;
+    // Turn and then delay slightly before next iteration
+    turn(movement);
     Sleep(0, UPDATE_DELAY);
   }
   halt();
--- src/linefollow.h
+++ src/linefollow.h
@@ -16,8 +16,9 @@ const int UPDATE_DELAY = 500000;
 const int WHITE_THRESHOLD = 255/2;
 // This number of white pixels is required for the bot to move
 const int STOP_COUNT = (IMAGE_SIZE_X / SAMPLE_STEPS);
-// The following are PID constants
+// The following are PID constants. Their sum should NEVER excede 1.
 const int K_P = 0.5;
+const int K_I = 0.2;
 
 void follow_line();
 

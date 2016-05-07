8cde48ec298ed213c0e4c247aea084bb90e033d2
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-07 18:06:18 +1200

    Implemented P of PID, cleaned up code, and improved line detection (untested)

--- src/linefollow.cpp
+++ src/linefollow.cpp
@@ -8,27 +8,32 @@
 void follow_line() {
   while (true) {
     take_picture();
-    int threshold_count = 0;
+    int white_count = 0;
     int movement = 0;
-    for (int i = SAMPLE_OFFSET; i < IMAGE_SIZE_X; i += SAMPLE_STEPS) {
-      int pixel_value = get_pixel(IMAGE_SIZE_Y / 2, i, COLOR_WHITE);
-      // Add to counter if a pixel is 'white enough' to be part of a line
-      if (pixel_value > STOP_THRESHOLD) {
-        threshold_count++;
+    // Get pixels from across the entire image, not just one line
+    for (int x = 0; x < IMAGE_SIZE_X; x += SAMPLE_STEPS) {
+      for (int y = 0; y < IMAGE_SIZE_Y; y += SAMPLE_STEPS) {
+        int pixel_value = get_pixel(y, x, COLOR_WHITE);
+        // Check if pixel is white
+        if (pixel_value > STOP_THRESHOLD) {
+          // Add to counter if a pixel is 'white enough' to be part of a line
+          white_count++;
+          // Now weigh the pixel into the movement counter
+          // I'm assuming y increases downwards when weighing
+          weight = y > (IMAGE_SIZE_Y / 2) ? 2 : 1;
+          movement += (i - IMAGE_SIZE_X / 2) * weight;
+        }
       }
-      movement += (i - IMAGE_SIZE_X / 2) * pixel_value;
     }
     // Break if there are not enough 'line worthy' pixels
-    if (threshold_count < STOP_COUNT) {
+    if (white_count < STOP_COUNT) {
       break;
     }
-    if (movement < 0) {
-      turn(-40);
-    } else if (movement > 0) {
-      turn(40);
-    } else {
-      reset_turn();
-    }
+    // Calculate how much to turn by
+    int pixels_x = x / SAMPLE_STEPS;
+    int pixels_y = (3 * (y / 2)) / SAMPLE_STEPS;
+    int movement_amount = (movement / (pixels_x * pixels_y)) * K_P;
+    turn(movement_amount);
     Sleep(0, UPDATE_DELAY);
   }
   halt();
--- src/linefollow.h
+++ src/linefollow.h
@@ -4,17 +4,20 @@
 const int IMAGE_SIZE_X = 320;
 const int IMAGE_SIZE_Y = 240;
 // The offset towards right at which sampling begins
+// Currently unused
 const int SAMPLE_OFFSET = (IMAGE_SIZE_X / 3) * 2;
 // The program samples every SAMPLE_STEPS pixels
-const int SAMPLE_STEPS = 10;
+const int SAMPLE_STEPS = 5;
 const int COLOR_WHITE = 3;
 // The time before the next update, in microseconds
 // Cannot excede 999999μs
 const int UPDATE_DELAY = 500000;
 // A pixel is considered white (a line) if it excedes this value
-const int STOP_THRESHOLD = 255/2;
+const int WHITE_THRESHOLD = 255/2;
 // This number of white pixels is required for the bot to move
-const int STOP_COUNT = (IMAGE_SIZE_X / SAMPLE_STEPS) / 10;
+const int STOP_COUNT = (IMAGE_SIZE_X / SAMPLE_STEPS);
+// The following are PID constants
+const int K_P = 0.5;
 
 void follow_line();
 

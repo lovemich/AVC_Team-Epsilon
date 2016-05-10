54ad9fb64eb88233717d7bc2c7af732ddb2ebb9f
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-10 13:18:29 +1200

    Made robot stop when force closed

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <csignal>
 
 #include "extern.h"
 #include "gate.h"
 #include "linefollow.h"
 
+void stop(int signum) {
+  set_motor(1, 0);
+  set_motor(2, 0);
+  exit(signum);
+}
+
 int main() {
+  signal(SIGINT, stop);
   init();
   open_gate();
   follow_line();
   return 0;
 }
-
 

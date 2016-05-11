c1bca82d17cf17d7621e874a9ada2f110b3e88f2
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-11 19:31:52 +1200

    Cleaned up comments and code in the line following. No functionality changed
    Signed-off-by: Devon Mortimer <devon.mortimer@gmail.com>

--- src/linefollow.h
+++ src/linefollow.h
@@ -3,24 +3,21 @@
 
 const int IMAGE_SIZE_X = 320;
 const int IMAGE_SIZE_Y = 240;
-// The offset towards right at which sampling begins
-// Currently unused
-const int SAMPLE_OFFSET = (IMAGE_SIZE_X / 3) * 2;
 // The program samples every SAMPLE_STEPS pixels
 const int SAMPLE_STEPS = 5;
 const int COLOR_WHITE = 3;
 // The time before the next update, in microseconds
-// Cannot excede 999999μs
+// Cannot exceed 999999μs
 const int UPDATE_DELAY = 1000;
 const int REVERSE_DELAY = 50000;
-// A pixel is considered white (a line) if it excedes this value
+// A pixel is considered white (a line) if it exceeds this value
 const int WHITE_THRESHOLD = 120;
 // This number of white pixels is required for the bot to move
 const int STOP_COUNT = (IMAGE_SIZE_X / SAMPLE_STEPS) * 1.5;
-// The following are PID constants. Their sum should probably not excede 1.
-const double K_P = 1;//1.45;
-const double K_I = 0.05;//0.05;
-const double K_D = 0.1;//0.3;
+// The following are PID constants.
+const double K_P = 1;
+const double K_I = 0.05;
+const double K_D = 0.1;
 
 void follow_line();
 int sample_image(int &white_count);

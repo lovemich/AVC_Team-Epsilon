48ab560a4b37e773e29f239b4faeb4cbbe6bb45e
LewisMcEwan <lewisjamesmcewan@gmail.com>
2016-05-18 11:04:47 +1200

    Added Dead End Code
    Initially set up the file so it could be created on personal laptop.
    Have now added actual basic code to build on.

--- src/find_dead_end.cpp
+++ src/find_dead_end.cpp
@@ -1,3 +1,36 @@
 #include <stdio.h>
 #include <cmath>
 #include <string.h>
+#include "extern.h"
+
+int main(){
+
+  init();     //initialises hardware
+
+  int frontSensor = 1;    //tells which sensor is which
+  int leftSensor = 2;
+  int rightSensor = 3;
+
+  int front;      //decalre integer variables to store the ADC data
+  int left;
+  int right;
+
+
+  front = read_analog(1);   //gather values from each sensor
+  printf("%d\n", front);
+  sleep(0, 5000000);
+
+  left = read_analog(2);
+  printf("%d\n", front);
+  sleep(0, 5000000);
+
+  right = read_analog(3);
+  printf("%d\n", front);
+  sleep(0, 5000000);
+
+  if(front<50 && left<50 && right<50){    //if all sensor values are small, then dead end
+    return true;
+  }
+
+
+}

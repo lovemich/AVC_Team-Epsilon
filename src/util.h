39c1ef483d01b64123b7160e91edc9d50130863c
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-23 21:54:55 +1200

    Corrected spelling mistake

--- src/util.h
+++ src/util.h
@@ -1,7 +1,7 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-// Evaluates to 1 if postive, -1 if negative, and 0 if either 0
+// Evaluates to 1 if positive, -1 if negative, and 0 if either 0
 #define sign(x) ((x > 0) - (x < 0))
 
 void stop(int signum);

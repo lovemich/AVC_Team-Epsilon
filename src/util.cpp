9ee3d92cbb29661553fefc33b4ea6dd45468e9e3
Devon Mortimer <devon.mortimer@gmail.com>
2016-05-25 09:37:50 +1200

    Moved inline to header

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <sys/time.h>
 
 #include "util.h"
 
@@ -10,13 +9,3 @@ void stop(int signum)
     exit(signum);
 }
 
-inline double get_seconds(timeval &prev_time)
-{
-    timeval curr_time;
-    gettimeofday(&curr_time, nullptr);
-    long d_sec = curr_time.tv_sec - prev_time.tv_sec;
-    int d_usec = curr_time.tv_usec - prev_time.tv_usec;
-    double d_time = d_usec / 1000000.0 + d_sec;
-    prev_time = curr_time;
-    return d_time;
-}

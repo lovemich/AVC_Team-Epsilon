71bd20d436f914c39721efa821b4c8c91193a745
LoganBarnett123 <logan.barnett123@gmail.com>
2016-05-26 11:50:18 +1200

    Added max/min values and slight turn constant

--- src/mazefollow.h
+++ src/mazefollow.h
@@ -2,15 +2,19 @@
 #define __MAZEFOLLOW_H__
 
 // Thresholds for IR sensors
-const int FRONT_MIN = 50;
-const int LEFT_MIN = 50;
-const int RIGHT_MIN = 50;
+const int LEFT_MIN = 10;
+const int RIGHT_MIN = 10;
+
+const int FRONT_MAX = 50;
+const int LEFT MAX = 50;
+const int RIGHT_MAX = 50;
 // Pins for IR sensors
 const int FRONT_PIN = 0;
 const int LEFT_PIN = 1;
 const int RIGHT_PIN = 2;
-// 90 degree turn values
+// Turn values
 const int SHARP_TURN = 80;
+const int SLIGHT_TURN = 10;
 // Number of tests
 const int TESTS = 10;
 // Wait time for turning

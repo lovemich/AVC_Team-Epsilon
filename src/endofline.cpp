1e151e64f5402111395578f21b40621e138adb5a
LoganBarnett123 <logan.barnett123@gmail.com>
2016-05-26 13:07:25 +1200

    Added IR sensor values to ensure maze readiness

--- src/endofline.cpp
+++ src/endofline.cpp
@@ -1,8 +1,8 @@
 //take_picture();
-// redCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 0);
-//int greenCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 1);
-//int blueCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 2);
+int redCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 0);
+int greenCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 1);
+int blueCheck = get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 2);
 
-if((get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 0) > 200) && (get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 1) < 80) && (get_pixel(IMAGE_SIZE_X / 2 + i, IMAGE_SIZE_Y / 2, 2) < 80)){
+if((redCheck > 200) && (greenCheck < 80) && (blueCheck < 80) && (read_analog(1) < 100) && (read_analog(2) < 100)){
   break;
   }
